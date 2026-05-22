729bfe33a8d880adf751c8c60128072871311918
MustbeThabz <tebogodipalett@gmail.com>
2026-05-22 18:27:32 +0200

    Serve landing page at root

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("node:path");
 
 const giftRoutes = require("./giftRoutes");
 const searchRoutes = require("./searchRoutes");
@@ -9,6 +10,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Landing page (Task 12 screenshot)
+const publicDir = path.join(__dirname, "public");
+app.use(express.static(publicDir));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(publicDir, "index.html"));
+});
 
 app.use(giftRoutes);
 // Search route: /api/search
